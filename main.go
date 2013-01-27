ff3c51b738a919ba515cc4f50b67d92a63c46ed1
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2013-01-27 23:14:11 +0530

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Give a function p which maps to fmt.Printf or fmt.Println 
+//based on the first input
+
+func main() {
+	fmt.Println("Enter a command")
+	fmt.Println("The result is:")
+}
